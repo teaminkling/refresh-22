dcfea2eea08fdede9dfaf0044f0141a27850dd8c
Thomas Wang <thomas@teaminkling.com>
2021-12-05 16:25:30 +1100

    Make a very minor code style change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,16 @@ import {Provider} from "react-redux";
 import {useStore} from "../store/store";
 import "../styles/globals.css";
 
+/**
+ * Main app component that provides access to the Redux store.
+ *
+ * @param Component the component
+ * @param {any} pageProps the page props
+ * @returns {JSX.Element} the {@link JSX.Element}
+ * @constructor
+ */
 const App = ({Component, pageProps}: AppProps): JSX.Element => {
   const store = useStore(pageProps.initialReduxState);
-
   return (
     <Provider store={store}>
       <Component {...pageProps} />
