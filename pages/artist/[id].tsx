1dd44c21a6f43f99decb91399fd314a3cccc8e63
Thomas Wang <thomas@teaminkling.com>
2021-12-24 15:37:48 +1100

    Get the route read working in dynamic routes

--- pages/artist/[id].tsx
+++ pages/artist/[id].tsx
@@ -2,10 +2,12 @@ import {useRouter} from "next/router";
 
 const Artist = () => {
   const router = useRouter();
-  const {pid} = router.query;
+  const {id} = router.query;
+
+  // First, we need to check that the artist actual
 
   return (
-    <p>Artist: {pid}</p>
+    <p>Artist: {id}</p>
   );
 };
 
