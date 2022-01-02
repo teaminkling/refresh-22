6513cd6c28ba70901dd6c87fe99a602ac982a3ec
Thomas Wang <thomas@teaminkling.com>
2022-01-02 11:23:47 +1100

    Try to fix social sharing

--- components/head.tsx
+++ components/head.tsx
@@ -36,6 +36,23 @@ export const RefreshHead = (props: RefreshHeadProps): JSX.Element => {
     <Head>
       <title>2022 Design Refresh - {props.subTitle}</title>
 
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
+      <meta name="theme-color" content="#7C7CE0" />
+
+      <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary_large_image" />
+
+      <meta property="og:site_name" content="Design Refresh" />
+      <meta property="og:title" content="Design Refresh" />
+      <meta name="twitter:title" content="Design Refresh" />
+
+      <meta property="og:url" content="https://refresh.fiveclawd.com/" />
+      <meta name="twitter:url" content="https://refresh.fiveclawd.com/" />
+
       <meta name="description" content={props.description} />
       <meta property="og:description" content={props.description} />
       <meta name="twitter:description" content={props.description} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,25 +22,6 @@ export const Document = () => {
           rel="stylesheet"
         />
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#7C7CE0" />
-
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-
-        <meta property="og:site_name" content="Design Refresh" />
-        <meta property="og:title" content="Design Refresh" />
-        <meta name="twitter:title" content="Design Refresh" />
-
-        <meta property="og:url" content="https://refresh.fiveclawd.com/" />
-        <meta name="twitter:url" content="https://refresh.fiveclawd.com/" />
-
         <style>{dom.css()}</style>
       </Head>
       <body className={"antialiased"}>
