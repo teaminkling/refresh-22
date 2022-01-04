0ee0fdf9695810b531d3636e3bb272c02fd8ea7e
Thomas Wang <thomas@teaminkling.com>
2022-01-05 01:42:17 +1100

    Add types and placeholders for the AWS S3 integration

--- api/src/types/environment.ts
+++ api/src/types/environment.ts
@@ -8,4 +8,8 @@ export default interface Environment {
   AUDIENCE: string;
   WEEKS_DISCORD_URL: string;
   WORKS_DISCORD_URL: string;
+  AWS_ACCESS_KEY_ID: string;
+  AWS_SECRET_ACCESS_KEY: string;
+  AWS_DEFAULT_REGION: string;
+  AWS_S3_BUCKET: string;
 }
