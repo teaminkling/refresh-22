97119e25680f29e49557daf1b4d229107d8f0de5
Thomas Wang <thomas@teaminkling.com>
2022-01-07 10:52:31 +1100

    Reduce appearance of 404s when posts are actually just loading

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,22 +1,46 @@
+import {useEffect, useState} from "react";
 import {Header, Paragraph, SubHeader} from "../components/typography";
 
-const NotFound = () => {
+/**
+ * A 404 page that shows after 2 seconds.
+ *
+ * @returns {JSX.Element} the element
+ * @constructor
+ */
+const NotFound = (): JSX.Element => {
+  const [isShown, setIsShown] = useState<boolean>(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsShown(true);
+    }, 2000);
+  }, []);
+
+  let response = <></>;
+  if (isShown) {
+    response = (
+      <>
+        <img
+          src={"/art/404.png"}
+          alt={"."}
+          className={"w-96 pt-16 m-auto"}
+        />
+        <Header>
+          404
+        </Header>
+        <SubHeader>
+          Not Found
+        </SubHeader>
+        <Paragraph>
+          Are you lost?
+        </Paragraph>
+      </>
+    );
+  }
+
   return <div className={"flex items-center justify-center h-full"}>
     <div className={"text-center pb-16"}>
-      <img
-        src={"/art/404.png"}
-        alt={"."}
-        className={"w-96 pt-16 m-auto"}
-      />
-      <Header>
-        404
-      </Header>
-      <SubHeader>
-        Not Found
-      </SubHeader>
-      <Paragraph>
-        Are you lost?
-      </Paragraph>
+      {response}
     </div>
   </div>;
 };
