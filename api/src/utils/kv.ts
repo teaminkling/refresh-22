bb6d320adc127c78c59c85d167a82dc5578cbf6b
Thomas Wang <thomas@teaminkling.com>
2022-01-07 00:07:55 +1100

    Ensure artist post count is updated on upload

--- api/src/utils/kv.ts
+++ api/src/utils/kv.ts
@@ -2,8 +2,11 @@
  * Utils related to key-value store operations.
  */
 
+import {ACTIVE_YEAR} from "../../../data/constants/setup";
+import Artist from "../../../data/core/Artist";
 import Work from "../../../data/core/Work";
 import {
+  ARTISTS,
   WORKS_WITH_ARTIST_INDEX,
   WORKS_WITH_ID_INDEX,
   WORKS_WITH_WEEK_INDEX,
@@ -58,4 +61,33 @@ export const placeWork = async (kv: KVNamespace, work: Work): Promise<void> => {
   worksWithoutIndex.push(work);
 
   await kv.put(WORKS_WITHOUT_INDEX, JSON.stringify(worksWithoutIndex));
+
+  // Now that the work is placed, increment the artist.
+
+  const rawBackendArtist: string | null = await kv.get(`${ARTISTS}/${work.artistId}`);
+  const backendArtist: Artist | undefined = (
+    rawBackendArtist ? JSON.parse(rawBackendArtist) : undefined
+  );
+
+  if (backendArtist) {
+    if (backendArtist.worksCount) {
+      backendArtist.worksCount++;
+    } else {
+      backendArtist.worksCount = 1;
+    }
+
+    // As usual, this might have race conditions.
+
+    const aggregateArtists: Record<string, Artist> = JSON.parse(
+      await kv.get(`${ARTISTS}/${ACTIVE_YEAR}`) || "{}"
+    );
+
+    aggregateArtists[work.artistId] = backendArtist;
+
+    await kv.put(`${ARTISTS}/${ACTIVE_YEAR}`, JSON.stringify(aggregateArtists));
+
+    // Also put it in a guaranteed consistent call.
+
+    await kv.put(`${ARTISTS}/${work.discordId}`, JSON.stringify(backendArtist));
+  }
 };
