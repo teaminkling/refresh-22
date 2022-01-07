9de844afe910484b237c92427756ed48056152bb
Thomas Wang <thomas@teaminkling.com>
2022-01-08 03:45:19 +1100

    Add a new method to get an Auth0 user's ID

--- utils/auth.ts
+++ utils/auth.ts
@@ -8,15 +8,26 @@ import {EDITORS} from "../data/constants/setup";
  * @returns {boolean} whether the given user is an editor
  */
 export const getIsEditor = (user?: User) => {
-  if (user && user.sub) {
-    const idParts: string[] = user.sub.split("|") || [];
+  if (user) {
+    const id: string | undefined = getUserId(user);
 
-    if (idParts.length > 0) {
-      const id: string = idParts[idParts.length - 1];
-
+    if (id) {
       return EDITORS.includes(id);
     }
   }
 
   return false;
+};
+
+/**
+ * From an Auth0 user, return an ID that is used in the system.
+ *
+ * @param {User} user the user
+ * @returns {string | undefined} the ID, if found
+ */
+export const getUserId = (user: User): string | undefined => {
+  const idParts: string[] = user.sub?.split("|") || [];
+  if (idParts.length > 0) {
+    return idParts[idParts.length - 1];
+  }
 };
