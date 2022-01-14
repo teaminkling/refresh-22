153208eca404fe29b09c88b5f847c0909a7d9ec4
Thomas Wang <thomas@teaminkling.com>
2022-01-14 20:00:18 +1100

    Update the beta page

--- pages/beta.tsx
+++ pages/beta.tsx
@@ -41,11 +41,11 @@ const BetaInfo = () => {
       </Paragraph>
 
       <UnorderedList>
+        <ListItem>Fix the meta/social sharing preview for individual works.</ListItem>
         <ListItem>Display artists correctly and consolidate them daily.</ListItem>
         <ListItem>A nicer drag-and-drop interface for editing socials.</ListItem>
         <ListItem>Display individual work galleries by maintaining aspect ratio.</ListItem>
         <ListItem>Make the weeks page more usable.</ListItem>
-        <ListItem>Stop flashing a 404 before loading a page.</ListItem>
       </UnorderedList>
 
       <Paragraph>
