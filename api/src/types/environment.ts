c7adcc74ecc190b31bc6ce61351e8c9154a9f133
Thomas Wang <thomas@teaminkling.com>
2024-09-16 04:34:03 +1000

    Remove more code and dependencies that definitely are no longer used

--- api/src/types/environment.ts
+++ api/src/types/environment.ts
@@ -1,18 +1,4 @@
-/**
- * The environment for this Worker.
- */
 export default interface Environment {
   REFRESH_KV: KVNamespace;
   ALLOWED_ORIGIN: string;
-  JWKS_URL: string;
-  AUDIENCE: string;
-  WEEKS_DISCORD_URL: string;
-  WORKS_DISCORD_URL: string;
-  AWS_ACCESS_KEY_ID: string;
-  AWS_SECRET_ACCESS_KEY: string;
-  AWS_DEFAULT_REGION: string;
-  AWS_S3_BUCKET: string;
-  TWIC_URL: string;
-  CDN_HOSTNAME: string;
-  ENVIRONMENT: string;
 }
