5758ceed03cd26cb9dea3d0f6eb95a38c1bd28f9
Thomas Wang <thomas@teaminkling.com>
2024-09-28 15:56:23 +1000

    Make images bigger on pages that support it

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -1,8 +1,6 @@
 export default function Head() {
   return (
     <>
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="@fiveclawd" />
       <meta name="theme-color" content="#7C7CE0" />
     </>
   );
